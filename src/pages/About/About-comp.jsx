a09ddf5cd45fe167f6f230d2d8c6dbf85aa25a07
GovindN75 <63858322+GovindN75@users.noreply.github.com>
2021-06-16 00:18:01 -0400

    update

--- src/pages/About/About-comp.jsx
+++ src/pages/About/About-comp.jsx
@@ -17,7 +17,7 @@ const About = () => {
           <Row className="pt-3 pb-5 align-items-center">
             <Col xs={12} md={6}>
               <Row className="justify-content-center mb-2 mr-2">
-                <Image className="profile justify-content-end" alt="profile" src={Profile} style={{borderRadius: "50%", borderStyle: "hidden"}}thumbnail fluid />
+                <Image className="profile justify-content-end" alt="profile" src={Profile} thumbnail fluid />
               </Row>
             </Col>
             <Col xs={12} md={6}>
