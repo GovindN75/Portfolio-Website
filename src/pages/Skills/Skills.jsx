2be1e700ac191994734a4f732b178b9c327d082b
GovindN75 <63858322+GovindN75@users.noreply.github.com>
2022-01-04 22:52:17 -0500

    Minor Fixes

--- src/pages/Skills/Skills.jsx
+++ src/pages/Skills/Skills.jsx
@@ -10,11 +10,11 @@ import "./skills-style.css";
 const Skills = () => {
   return (
     <div className="pt-3 pb-3" id="skills">
-      <h1 className="text-center font-details-b pb-4" style={{color: "#99774f"}}>Things I am sometimes good at</h1>
+      <h1 className="text-center font-details-b pb-4" style={{color: "#99774f"}}>My Skillset</h1>
       <CardDeck>
         <Row className="d-flex justify-content-around">
           <Col md={4}>
-            <Card className="focus mt-2 mb-2" style={{backgroundColor: "transparent", border: "1px white solid"}}>
+            <Card className="focus mt-2 mb-2" style={{backgroundColor: "#f0f8ff"}}>
               <Card.Body>
                 <Card.Title className="text-center card-title" style={{color: "#99774f"}}>Programming Languages</Card.Title>
                 <hr />
@@ -32,7 +32,7 @@ const Skills = () => {
           </Col>
 
           <Col md={4}>
-            <Card className="focus mt-2 mb-2" style={{backgroundColor: "transparent", border: "1px white solid"}}>
+            <Card className="focus mt-2 mb-2" style={{backgroundColor: "#f0f8ff"}}>
               <Card.Body>
                 <Card.Title className="text-center  card-title" style={{color: "#99774f"}}>Web Development</Card.Title>
                 <hr />
@@ -50,14 +50,14 @@ const Skills = () => {
           </Col>
 
           <Col md={4}>
-            <Card className="focus mt-2 mb-2" style={{backgroundColor: "transparent", border: "1px white solid"}}>
+            <Card className="focus mt-2 mb-2" style={{backgroundColor: "#f0f8ff"}}>
               <Card.Body>
                 <Card.Title className="text-center card-title" style={{color: "#99774f"}}>Tools and Technologies</Card.Title>
                 <hr />
                 <Card.Text className="card-text d-flex justify-content-start flex-column">
                   {skills.misc.map((skill, index) => (
                     <span className="p-2" key={index}>
-                      <a href={skill.link} target="_blank" rel="noopener noreferrer" style={{color: "#99774f"}}>
+                      <a className="text-dark text-decoration-none" href={skill.link} target="_blank" rel="noopener noreferrer" style={{color: "#99774f"}}>
                         <Image src={skill.img} alt={skill.img_alt} rounded className="image-style m-1"></Image> {skill.name}
                       </a>
                     </span>
